38fdee927ab119ad2171bee77fb2e850dff9bd7a
NicoPaez <nicopaez@gmail.com>
2015-05-29 04:21:08 -0300

    Checkstyle replaced by PMD

--- src/fiuba/algo3/ejemplo1/Foo.java
+++ src/fiuba/algo3/ejemplo1/Foo.java
@@ -1,7 +1,14 @@
 package fiuba.algo3.ejemplo1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Foo {
 
+	// Los generic son chotos. No los quiero
+	@SuppressWarnings("rawtypes")
+	ArrayList array;
+	
 	public String doFoo() {
 		return "Foo";
 	}
