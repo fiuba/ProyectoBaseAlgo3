999e485d795af2e75870e76b1a351bb3df53fad0
NicoPaez <nicopaez@gmail.com>
2015-06-28 17:32:11 -0300

    Logica para ejecutar la app

--- src/fiuba/algo3/ejemplo1/Foo.java
+++ src/fiuba/algo3/ejemplo1/Foo.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 
 public class Foo {
 
-	// Los generic son chotos. No los quiero
-	@SuppressWarnings("rawtypes")
-	ArrayList array;
+	public static void main(String[] args) {
+		System.out.println("Foo bye");
+	}
 	
 	public String doFoo() {
 		return "Foo";
